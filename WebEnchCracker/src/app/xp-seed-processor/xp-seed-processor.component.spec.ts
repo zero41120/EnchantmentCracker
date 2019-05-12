c8579db22cb51e8f959ff8b9729f37292b8c46b6
zero41120 <devtlin34@gmail.com>
2019-05-12 14:18:55 -0700

    Add unit test for comput enchantability

--- WebEnchCracker/src/app/xp-seed-processor/xp-seed-processor.component.spec.ts
+++ WebEnchCracker/src/app/xp-seed-processor/xp-seed-processor.component.spec.ts
@@ -20,9 +20,26 @@ describe('XpSeedProcessorComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should compute enchantability', () => {
-    const fixture = TestBed.createComponent(XpSeedProcessorComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.getEnchantability()).toEqual(0);
+  describe('Compute the enchantability using Java-Random typed class', () => {
+    const Random = require('java-random');
+    it('should compute enchantability', () => {
+      const fixture = TestBed.createComponent(XpSeedProcessorComponent);
+      const app = fixture.debugElement.componentInstance;
+      // Use java 1.8.0_31 as reference
+      const seed = 28825252;
+      let bookshelves = 0;
+      const ctrlRng = new Random(seed);
+      let controlResult = 1 + ctrlRng.nextInt(8) + ctrlRng.nextInt(1);
+      const testRng = new Random(seed);
+      let result = app.getEnchantability(testRng, bookshelves);
+      expect(result).toEqual(controlResult);
+
+      bookshelves = 15;
+      ctrlRng.setSeed(seed);
+      testRng.setSeed(seed);
+      controlResult = 1 + (15 >> 1) + ctrlRng.nextInt(8) + ctrlRng.nextInt(16);
+      result = app.getEnchantability(testRng, bookshelves);
+      expect(result).toEqual(controlResult);
+    });
   });
 });
